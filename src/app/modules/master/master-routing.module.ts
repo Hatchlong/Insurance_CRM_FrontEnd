fb1548eda71325e8fd4f84b4c1273c5aab4134f4
Himanshu sahu <himanshusahu7500@gmail.com>
2023-12-18 19:15:10 +0530

    vendor master

--- src/app/modules/master/master-routing.module.ts
+++ src/app/modules/master/master-routing.module.ts
@@ -6,11 +6,8 @@ import { AddProductComponent } from './components/product/add-product/add-produc
 import { AddVendorComponent } from './components/vendor/add-vendor/add-vendor.component';
 import { CustomerListComponent } from './components/customer-master/customer-list/customer-list.component';
 import { AddCustomerComponent } from './components/customer-master/add-customer/add-customer.component';
-<<<<<<< HEAD
 import { EditVendorComponent } from './components/vendor/edit-vendor/edit-vendor.component';
-=======
 import { EditProductComponent } from './components/product/edit-product/edit-product.component';
->>>>>>> 835ab9e2217e270ccfd0d1dcabe22e3f035eeaab
 
 const routes: Routes = [{
   path:'product',
