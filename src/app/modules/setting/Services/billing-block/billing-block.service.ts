b6fa01c6b7316d48e9529c897fe6fec9ed26f770
thulasidassb8 <thulasidassb8@gmail.com>
2023-12-27 10:57:25 +0530

    url changed

--- src/app/modules/setting/Services/billing-block/billing-block.service.ts
+++ src/app/modules/setting/Services/billing-block/billing-block.service.ts
@@ -11,32 +11,32 @@ export class BillingBlockService {
   constructor(private http: HttpClient) { }
  
   createBillingBlock(data: any) {
-    return this.http.post('http://localhost:4000/api/master/billingblock/create', data).toPromise()
+    return this.http.post('http://54.151.187.67:4000/api/master/billingblock/create', data).toPromise()
   }
 
   getAllBillingBlockDetails() {
-    return this.http.get('http://localhost:4000/api/master/billingblock/getAll').toPromise()
+    return this.http.get('http://54.151.187.67:4000/api/master/billingblock/getAll').toPromise()
   }
 
   getAllBillingBlockPage(skip?:any, itemsPerPage?:any) {
-    return this.http.get(`http://localhost:4000/api/master/billingblock/getAll/`).toPromise()
+    return this.http.get(`http://54.151.187.67:4000/api/master/billingblock/getAll/`).toPromise()
   }
 
   singleBillingBlockDetails(id: any) {
-    return this.http.get(`http://localhost:4000/api/master/billingblock/get/${id}`).toPromise()
+    return this.http.get(`http://54.151.187.67:4000/api/master/billingblock/get/${id}`).toPromise()
   }
 
   updatedBillingBlockDetails(data: any) {
-    return this.http.put(`http://localhost:4000/api/master/billingblock/update/${data._id}`, data).toPromise()
+    return this.http.put(`http://54.151.187.67:4000/api/master/billingblock/update/${data._id}`, data).toPromise()
   }
 
   updateBillingBlockMany(data: any) {
-    return this.http.put(`http://localhost:4000/api/master/billingblock/update`, data).toPromise()
+    return this.http.put(`http://54.151.187.67:4000/api/master/billingblock/update`, data).toPromise()
   }
 
  
   fileUploadBillingBlock(data: any) {
-    return this.http.post(`http://localhost:4000/api/master/billingblock/upload`, data).toPromise()
+    return this.http.post(`http://54.151.187.67:4000/api/master/billingblock/upload`, data).toPromise()
   }
   exportToExcel(data: any[], fileName: string, sheetName: string): void {
     data.map((el: any) => {
--- src/app/modules/setting/Services/distibution-channel/distibution-channel.service.ts
+++ src/app/modules/setting/Services/distibution-channel/distibution-channel.service.ts
@@ -14,31 +14,31 @@ export class DistibutionChannelService {
  
 
   createDistibutionChannelDetails(data:any){
-    return this.http.post('http://localhost:4000/api/master/distributionChannel/create', data).toPromise()
+    return this.http.post('http://54.151.187.67:4000/api/master/distributionChannel/create', data).toPromise()
   }
  
   getAllDistibutionChannelDetails(){
-    return this.http.get('http://localhost:4000/api/master/distributionChannel/getAll').toPromise()
+    return this.http.get('http://54.151.187.67:4000/api/master/distributionChannel/getAll').toPromise()
   }
  
   singleDistibutionChannel(id:any){
-    return this.http.get(`http://localhost:4000/api/master/distributionChannel/get/${id}`).toPromise()
+    return this.http.get(`http://54.151.187.67:4000/api/master/distributionChannel/get/${id}`).toPromise()
   } 
  
   updateDistibutionChannel(data:any){
-    return this.http.put(`http://localhost:4000/api/master/distributionChannel/update/${data._id}`, data).toPromise()
+    return this.http.put(`http://54.151.187.67:4000/api/master/distributionChannel/update/${data._id}`, data).toPromise()
   }
 
   fileUploadXlsx(data:any){
-    return this.http.post(`http://localhost:4000/api/master/distributionChannel/upload`, data).toPromise()
+    return this.http.post(`http://54.151.187.67:4000/api/master/distributionChannel/upload`, data).toPromise()
   }
 
   updatedManydistributionChannelDetails(data:any){
-    return this.http.put(`http://localhost:4000/api/master/distributionChannel/update`, data).toPromise()
+    return this.http.put(`http://54.151.187.67:4000/api/master/distributionChannel/update`, data).toPromise()
   }
 
   getAlldistributionChannelDetailsPage(skip?: any, itemsPerPage?: any){
-    return this.http.get(`http://localhost:4000/api/master/distributionChannel/getAll/${skip}/${itemsPerPage}`).toPromise()
+    return this.http.get(`http://54.151.187.67:4000/api/master/distributionChannel/getAll/${skip}/${itemsPerPage}`).toPromise()
   }
 
   exportToExcel(data: any[], fileName: string, sheetName: string): void {
